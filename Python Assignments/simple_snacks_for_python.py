e2c9f5ae13a8d39c62a5058c400532da5aef682d
Dumebi Onwusah <mebi@Dumebis-MacBook-Air.local>
2025-12-03 10:05:28 +0100

    First commit

---
+++ Python Assignments/simple_snacks_for_python.py
@@ -0,0 +1,46 @@
+#Chukwudumebi Onwusah
+
+#1 
+name = input("Enter your name")
+
+if name == "Alex":
+    print("Hellow, friend!")
+else:
+    print("Hello stranger!")
+print()
+
+
+#2
+pet = input("Pick a pet! ('dog' or 'cat') ")
+if pet == "dog":
+    print("Woof! Dogs are awesome!")
+else:
+    print("Cool choice, but I love dogs!”")
+print()
+
+
+#3
+favorite_number = 7
+user_number = int(input("Guess my favorite number: "))
+
+if user_number == favorite_number:
+    print("Thats my favorite number!")
+else:
+    print("Nice try. guess again!")
+print()
+
+
+#4
+for i in range(1, 6):
+    print("Number", i)
+print()
+
+
+#5
+user_input = int(input("Enter a number!: "))
+
+if user_input % 2 == 0:
+    print("EVEN!")
+else:
+    print("ODD!")
+print()
